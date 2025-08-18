0ecf1c2a659c49020c80f1966db80f3fcf6f79d3
Dario Passariello <dariopassariello@gmail.com>
2025-08-17 21:01:53 -0600

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="./types/memorio.d.ts" />
+/// <reference path="./types/observer.d.ts" />
+/// <reference path="./types/state.d.ts" />
+/// <reference path="./types/store.d.ts" />
