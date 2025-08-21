b71bcec24bc5b179d67a15ea00cef2c45cd08606
Dario Passariello <dariopassariello@gmail.com>
2025-08-20 21:20:52 -0600

    Update init.ts

--- init.ts
+++ init.ts
@@ -1,5 +1,11 @@
+
 ///////////////////////////////////////////////////////////////////////////////
 // LOAD SCRIPTS
+
+// import "./types/memorio.d.ts"
+// import "./types/observer.d.ts"
+// import "./types/state.d.ts"
+// import "./types/store.d.ts"
 
 require('./config/global')
 require('./config/dispatch')
@@ -7,3 +13,5 @@ require('./config/dispatch')
 require('./functions/state')
 require('./functions/observer')
 require('./functions/store')
+
+// import "memorio"
