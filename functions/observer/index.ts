d054378aa62686a5247f78bc474198f2d3b9ad8f
Dario Passariello <dariopassariello@gmail.com>
2025-09-30 23:08:10 -0600

    Update index.ts

--- functions/observer/index.ts
+++ functions/observer/index.ts
@@ -1,131 +1,127 @@
-if (!Object.getOwnPropertyDescriptor(globalThis, 'observer')) {
+if (!globalThis.observer) globalThis.observer = null
 
-  if (!globalThis.observer) globalThis.observer = null
+Object.defineProperty(
+  globalThis,
+  'observer',
+  {
+    enumerable: false,
+    configurable: true
+  }
+)
 
-  Object.defineProperty(
-    globalThis,
-    'observer',
-    {
-      enumerable: false,
-      configurable: true
-    }
-  )
+///////////////////////////////////////////////////////////////////////////////
+// START OBSERVER FUNCTION
 
-  ///////////////////////////////////////////////////////////////////////////////
-  // START OBSERVER FUNCTION
+observer = (s, cb = null, option = true) => {
 
-  observer = (s, cb = null, option = true) => {
-
-    // CHECK IF 'STATE.' OR 'STORE.' EXIST
-    try {
-      const checkStateOrStore = (s: string) => {
-        const t = s.split('.')
-        if (t[0] !== 'state') {
-          console.error(`Observer Error: You need to declare 'state.' or 'store.'. The '${s}' string is incorrect!`)
-          return false
-        }
-        return true
+  // CHECK IF 'STATE.' OR 'STORE.' EXIST
+  try {
+    const checkStateOrStore = (s: string) => {
+      const t = s.split('.')
+      if (t[0] !== 'state') {
+        console.error(`Observer Error: You need to declare 'state.' or 'store.'. The '${s}' string is incorrect!`)
+        return false
       }
-
-      if (!checkStateOrStore(s)) return
-    } catch (error) {
-      console.error(error)
-      return
+      return true
     }
 
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    // LIST ALL ARRAY FROM STATE
-    if (!s && !cb) {
-      console.error('Observer Error: You need to setup observer correctly, Some parameters are missed!')
-      return
-    }
-
-    ////////////////////////////
-
-    // CHECK
-    if (!s && cb) {
-      console.error(`Observer Error: You need to declare what state need to be monitored as string like 'state.test'.`)
-      return
-    }
-
-    ////////////////////////////
-
-    // RECALL
-    if (s && !cb) {
-      globalThis.memorio.dispatch.listen(
-        String(s),
-        {
-          detail: {
-            name: String(s)
-          }
-        } as any
-      )
-
-      console.debug('called: ', s)
-      return
-    }
-
-    ////////////////////////////
-
-    // SETUP
-    if (s && cb) {
-      if (typeof s !== 'string' || typeof cb !== 'function') {
-        console.error(`Observer Error: name of state need to be a 'string' like 'state.test' and the callback need to be a 'function'`)
-        return
-      }
-
-      globalThis.memorio.dispatch.listen(s, cb, option)
-      return
-    }
+    if (!checkStateOrStore(s)) return
+  } catch (error) {
+    console.error(error)
+    return
   }
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  Object.defineProperties(
-    observer,
-    {
+  // LIST ALL ARRAY FROM STATE
+  if (!s && !cb) {
+    console.error('Observer Error: You need to setup observer correctly, Some parameters are missed!')
+    return
+  }
 
-      /**
-       * See the list of all your observer (eventListener) actually installed.
-       * @returns The list of all installed observers.
-       */
-      list: {
-        get: () => globalThis.events
-      },
+  ////////////////////////////
 
-      /////////////////////////////////////////////////////
+  // CHECK
+  if (!s && cb) {
+    console.error(`Observer Error: You need to declare what state need to be monitored as string like 'state.test'.`)
+    return
+  }
 
-      /**
-       * Remove an observer.
-       * @param name The name of the observer to remove.
-       */
-      remove: {
-        value: (name: any) => {
-          if (!name) return
-          globalThis.events[name] = ''
-          return
+  ////////////////////////////
+
+  // RECALL
+  if (s && !cb) {
+    globalThis.memorio.dispatch.listen(
+      String(s),
+      {
+        detail: {
+          name: String(s)
         }
-      },
+      } as any
+    )
 
-      /////////////////////////////////////////////////////
+    console.debug('called: ', s)
+    return
+  }
 
-      /**
-       * Remove all observers.
-       * @returns void
-       */
-      removeAll: {
-        get: () => {
-          Object.entries(observer.list).forEach((el: any) => {
-            globalThis.events[el[0]]
-          })
-          return
-        }
-      }
+  ////////////////////////////
 
+  // SETUP
+  if (s && cb) {
+    if (typeof s !== 'string' || typeof cb !== 'function') {
+      console.error(`Observer Error: name of state need to be a 'string' like 'state.test' and the callback need to be a 'function'`)
+      return
     }
-  )
 
-  Object.freeze(observer)
-
+    globalThis.memorio.dispatch.listen(s, cb, option)
+    return
+  }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Object.defineProperties(
+  observer,
+  {
+
+    /**
+     * See the list of all your observer (eventListener) actually installed.
+     * @returns The list of all installed observers.
+     */
+    list: {
+      get: () => globalThis.events
+    },
+
+    /////////////////////////////////////////////////////
+
+    /**
+     * Remove an observer.
+     * @param name The name of the observer to remove.
+     */
+    remove: {
+      value: (name: any) => {
+        if (!name) return
+        globalThis.events[name] = ''
+        return
+      }
+    },
+
+    /////////////////////////////////////////////////////
+
+    /**
+     * Remove all observers.
+     * @returns void
+     */
+    removeAll: {
+      get: () => {
+        Object.entries(observer.list).forEach((el: any) => {
+          globalThis.events[el[0]]
+        })
+        return
+      }
+    }
+
+  }
+)
+
+Object.freeze(observer)
